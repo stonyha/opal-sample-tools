675c89c0f608adc0a5827d662cf55a9b1c9d601e
Ha Huynh <ha.huynh@niteco.se>
2025-11-20 17:40:46 +0700

    fix bug

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ opal_tools_service = ToolsService(app)
 # Import your tool here
 from src.tools.url_slug_generator import generate_url_slug
 from src.tools.broken_link_checker import broken_link_checker
-#from src.tools.tech_stack_checker import tech_stack_discovery
+from src.tools.tech_stack_discovery import tech_stack_discovery
